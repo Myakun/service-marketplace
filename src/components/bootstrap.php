18b539f296ab0cb33395f4f65ef5f9340a86f029
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-09-01 17:35:34 +0200

    Restructure project layout

--- src/components/bootstrap.php
+++ src/components/bootstrap.php
@@ -6,8 +6,8 @@ defined('APP_ENV') || define('APP_ENV', getenv('APP_ENV'));
 defined('YII_DEBUG') || define('YII_DEBUG', APP_ENV == 'dev');
 defined('YII_ENV') || define('YII_ENV', APP_ENV);
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 if (YII_DEBUG) {
     error_reporting(E_ALL);
@@ -15,4 +15,4 @@ if (YII_DEBUG) {
 }
 
 Yii::setAlias('app', __DIR__ . '/../');
-Yii::setAlias('@bower', __DIR__ . '/../vendor/bower-asset');
+Yii::setAlias('@bower', __DIR__ . '/../../vendor/bower-asset');
