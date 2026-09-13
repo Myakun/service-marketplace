8b4d8e0db9cb5b7584d0715a83f35ca28a6b5485
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-09-13 15:22:02 +0200

    Add PHPUnit

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,7 +7,7 @@ use PhpCsFixer\Finder;
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $finder = Finder::create()
-    ->in(__DIR__ . '/src')
+    ->in([__DIR__ . '/src', __DIR__ . '/tests'])
     ->name('*.php');
 
 return new Config()
