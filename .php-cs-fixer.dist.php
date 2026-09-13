bf699a3c2a0d3e7289520777dd8cd273b624503b
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-09-13 13:26:02 +0200

    Add PHP CS Fixer to follow coding standards

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->name('*.php');
+
+return (new Config())
+    ->setCacheFile(__DIR__ . '/var/cache/php-cs-fixer.cache')
+    ->setFinder($finder)
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@PHP85Migration' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'declare_strict_types' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => true,
+    ]);
+
--- Makefile
+++ Makefile
@@ -13,6 +13,12 @@ docker-rebuild:
 	docker compose build
 	docker compose up -d --remove-orphans
 
+phpcs:
+	docker exec -w /var/www/app service-marketplace-php /usr/local/bin/php vendor/bin/php-cs-fixer fix --dry-run --diff
+
+phpcs-fix:
+	docker exec -w /var/www/app service-marketplace-php /usr/local/bin/php vendor/bin/php-cs-fixer fix
+
 phpstan:
 	docker exec -w /var/www/app service-marketplace-php /usr/local/bin/php vendor/bin/phpstan analyse --memory-limit=1G
 
@@ -32,4 +38,3 @@ yii-migrate-down-all:
 
 yii-migrate-up:
 	docker exec service-marketplace-php /usr/local/bin/php /var/www/app/src/yii.php migrate/up --interactive=0
-
