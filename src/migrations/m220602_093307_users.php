848a867734afd03bfb32b96c509273ff10a91e3b
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-09-13 23:24:32 +0200

    Add i18n support, "name" field constant length

--- src/migrations/m220602_093307_users.php
+++ src/migrations/m220602_093307_users.php
@@ -7,32 +7,32 @@ use yii\db\Migration;
 
 class m220602_093307_users extends Migration
 {
-	public function safeUp(): bool
-	{
-		$this->createTable(User::tableName(), [
-			'id' => $this->primaryKey(),
-			'email' => $this->string(255)->notNull()->unique(),
-			'name' => $this->string(User::NAME_MAX_LENGTH)->notNull(),
-			'password' => $this->string(100)->notNull(),
+    public function safeUp(): bool
+    {
+        $this->createTable(User::tableName(), [
+            'id' => $this->primaryKey(),
+            'email' => $this->string(255)->notNull()->unique(),
+            'name' => $this->string(100)->notNull(),
+            'password' => $this->string(100)->notNull(),
             'created_at' => $this->dateTime()->notNull(),
             'created_by' => $this->integer()->defaultValue(null)
-		]);
+        ]);
 
         $user = new User();
         $user->setAttributes([
             'email' => 'admin@example.com',
-            'name' => 'Администратор',
+            'name' => Yii::t('app', 'Administrator'),
             'password' => 'admin'
         ]);
         $user->save();
 
         return true;
-	}
+    }
 
-	public function safeDown(): bool
-	{
-		$this->dropTable(User::tableName());
+    public function safeDown(): bool
+    {
+        $this->dropTable(User::tableName());
 
         return true;
-	}
+    }
 }
