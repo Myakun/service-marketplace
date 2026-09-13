a0fb36570c4c0be68bf2dc3fde26b3fd5aa5f915
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-09-13 21:51:08 +0200

    Formatting and default name change

--- src/config/params.example.php
+++ src/config/params.example.php
@@ -8,7 +8,7 @@ return [
     'mailer' => [
         'encryption' => '${APP_MAILER_ENCRYPTION}',
         'host' => '${APP_MAILER_HOST}',
-        'port' => (int) '${APP_MAILER_PORT}',
+        'port' => (int)'${APP_MAILER_PORT}',
         'username' => '${APP_MAILER_USER}',
         'password' => '${APP_MAILER_PASS}',
     ],
@@ -17,14 +17,14 @@ return [
         'host' => '${APP_MYSQL_HOST}',
         'username' => '${APP_MYSQL_USER}',
         'password' => '${APP_MYSQL_PASS}',
-        'port' => (int) '${APP_MYSQL_PORT}',
+        'port' => (int)'${APP_MYSQL_PORT}',
     ],
     'MySQL1' => [
         'database' => 'u1712448_default',
         'host' => '31.31.196.138',
         'username' => 'u1712448_default',
         'password' => 'wxmmIil7VOr4P9H3',
-        'port' => (int) '3306',
+        'port' => (int)'3306',
     ],
     'params' => [
         'contacts' => [
@@ -32,7 +32,7 @@ return [
             'phone' => '${APP_CONTACTS_PHONE}',
         ],
         'emailFrom' => '${APP_MAILER_USER}',
-        'siteName' => 'Look.Clean',
+        'siteName' => 'Service Marketplace',
     ],
     'reCaptcha' => [
         'siteKey' => '${APP_RECAPTCHA_SITE_KEY}',
