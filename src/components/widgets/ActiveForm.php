e342cfe5742395b170135c598f9aa620a158450d
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-09-14 09:44:17 +0200

    Format code

--- src/components/widgets/ActiveForm.php
+++ src/components/widgets/ActiveForm.php
@@ -6,18 +6,18 @@ namespace app\components\widgets;
 
 class ActiveForm extends \kartik\widgets\ActiveForm
 {
-	public $enableClientScript = false;
+    public $enableClientScript = false;
 
-	public $enableClientValidation = false;
+    public $enableClientValidation = false;
 
-	public $errorSummaryCssClass = 'alert alert-danger';
+    public $errorSummaryCssClass = 'alert alert-danger';
 
-	public function errorSummary($models, $options = []): string
-	{
-		if (!isset($options['header'])) {
-			$options['header'] = '';
-		}
+    public function errorSummary($models, $options = []): string
+    {
+        if (!isset($options['header'])) {
+            $options['header'] = '';
+        }
 
-		return parent::errorSummary($models, $options);
-	}
+        return parent::errorSummary($models, $options);
+    }
 }
