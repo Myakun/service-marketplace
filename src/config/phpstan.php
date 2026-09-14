4e4119da40d3e7f21971e35839c6cbde967ec352
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-09-14 12:09:38 +0200

    Drop behaviors map from PHPStan config in favour of @mixin

--- src/config/phpstan.php
+++ src/config/phpstan.php
@@ -4,28 +4,17 @@ declare(strict_types=1);
 
 /**
  * Application config for static analysis only.
- * Consumed by yii2-extensions/phpstan to resolve the types of Yii::$app components,
- * params, and model behaviors.
+ * Consumed by yii2-extensions/phpstan to resolve the types of Yii::$app components
+ * and params.
  */
 
-use app\components\behaviors\BlameableBehavior as ComponentsBlameableBehavior;
 use app\components\i18n\Formatter;
-use app\models\Category;
-use app\models\Customer;
-use app\models\Order;
-use app\models\Partner;
-use app\models\Price;
-use app\models\Service;
-use app\models\User;
 use himiklab\yii2\recaptcha\ReCaptchaConfig;
-use yii\behaviors\BlameableBehavior;
-use yii\behaviors\TimestampBehavior;
 use yii\caching\FileCache;
 use yii\db\Connection;
 use yii\rbac\DbManager;
 use yii\swiftmailer\Mailer;
 use yii\web\IdentityInterface;
-use yii2tech\ar\position\PositionBehavior;
 
 return [
     'params' => [
@@ -36,15 +25,6 @@ return [
         ],
         'emailFrom' => '',
         'siteName' => '',
-    ],
-    'behaviors' => [
-        Category::class => [BlameableBehavior::class, PositionBehavior::class, TimestampBehavior::class],
-        Customer::class => [TimestampBehavior::class],
-        Order::class => [TimestampBehavior::class],
-        Partner::class => [TimestampBehavior::class],
-        Price::class => [TimestampBehavior::class],
-        Service::class => [BlameableBehavior::class, PositionBehavior::class, TimestampBehavior::class],
-        User::class => [ComponentsBlameableBehavior::class, TimestampBehavior::class],
     ],
     'components' => [
         'authManager' => [
