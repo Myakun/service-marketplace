7804665081df5320cd02649f30bef270de1f1265
Anatolii Zanosov <wegotmoves1799@gmail.com>
2026-09-14 09:45:06 +0200

    Declare errorSummary() model list as array<Model> for PHPStan

--- src/components/widgets/ActiveForm.php
+++ src/components/widgets/ActiveForm.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace app\components\widgets;
 
+use yii\base\Model;
+
 class ActiveForm extends \kartik\widgets\ActiveForm
 {
     public $enableClientScript = false;
@@ -12,6 +14,10 @@ class ActiveForm extends \kartik\widgets\ActiveForm
 
     public $errorSummaryCssClass = 'alert alert-danger';
 
+    /**
+     * @param Model|array<Model> $models
+     * @param array<string, mixed> $options
+     */
     public function errorSummary($models, $options = []): string
     {
         if (!isset($options['header'])) {
